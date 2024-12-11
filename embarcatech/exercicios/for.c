a6299050ec3d74fe5a63fc42a37214b6acc984ab
Matheus Braga <sbmatheus20@gmail.com>
2024-12-11 19:31:15 -0300

    repeticao-for

---
+++ embarcatech/exercicios/for.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+   // FOR
+
+    int num;
+
+    for (num = 0; num < 20; num = num + 1)
+    {
+      printf("My name is Bart Simpsons \n");
+    }
+
+    return 0;
+}
