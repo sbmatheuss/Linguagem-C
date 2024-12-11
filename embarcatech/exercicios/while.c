9728b028053bc8eafec1c87f3c40064c432f3480
Matheus Braga <sbmatheus20@gmail.com>
2024-12-11 19:41:32 -0300

    repeticao-while

---
+++ embarcatech/exercicios/while.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+  // WHILE
+
+  int num;
+  num = 0;
+
+  while(num < 10) 
+  {
+    printf("My name is Homer \n");
+    num++; // ou: num = num + 1
+  }
+    return 0;
+}
